32397cdf063f93ee5e786399a1d2946b431cf84c
Данилова Вероника <dveronika682@gmail.com>
2024-11-03 13:54:53 +0500

    Первое изменение в коде

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -6,10 +6,14 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-        
-            Console.WriteLine("Калькулятор");
+            Console.WriteLine("Kалькулятор");
 
-           
+            Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
         }
     }
 }
