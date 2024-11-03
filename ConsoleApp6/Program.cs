ffe103205c0c472c1261924044dda3c8a0f3a168
Данилова Вероника <dveronika682@gmail.com>
2024-11-03 13:43:56 +0500

    Начало написание кода

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Калькулятор");
+
+           
+        }
+    }
+}
